664f68db043205bc863d77f573e652f4237a5050
Yadir <yadamuoz@misena.edu.co>
2024-10-23 18:36:50 -0500

    subida inicial

---
+++ build/prueba/index.js
@@ -0,0 +1,15 @@
+let stateUser = true;
+console.log(stateUser, 'stateUsers');
+let modeloCar = 145;
+console.log(modeloCar, 'modeloCar');
+let tienda = {
+    producto: 'arroz',
+    precio: 1000
+};
+function calculateCircleArea(radio, pi = 3.14) {
+    return (pi * radio * radio);
+}
+let radio = 425;
+const areaCircle = calculateCircleArea(radio);
+console.log('el area del circulo es: ', areaCircle);
+//# sourceMappingURL=index.js.map
