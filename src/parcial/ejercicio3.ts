b8718be7eae2855db0c6a383637c6450bdc3919d
Yadir <yadamuoz@misena.edu.co>
2024-10-23 22:37:43 -0500

    punto 3 avance

--- src/parcial/ejercicio3.ts
+++ src/parcial/ejercicio3.ts
@@ -1,1 +1,50 @@
- 
+ // Crea una interfaz llamada ContacInfo 
+
+ interface ContacInfo {
+    id: number,
+    name: string,
+    phoneNumber: number
+ }
+
+function valPhone(phoneNumber: number): boolean {
+    const phone = phoneNumber.toString();
+    if (phone.length === 10) {
+        return true;
+    }
+    return false;
+}
+
+class Contact implements ContacInfo {
+    id: number;
+    name: string;
+    phoneNumber: number;
+
+    constructor(id: number, name: string, phoneNumber: number) {
+        this.id = id;
+        this.name = name;
+        if (valPhone(phoneNumber)) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            throw new Error('numero invalido');
+        }
+    }
+
+// leer o obtener datos
+    getContact(): ContacInfo {
+        return {
+            id: this.id,
+            name: this.name,
+            phoneNumber: this.phoneNumber
+        };
+    }
+// actualizar
+    updateContact(name: string, phoneNumber: number): void {
+        this.name = name;
+        if (valPhone(phoneNumber)) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            throw new Error('numero invalido');
+        }
+    }
+}
+
