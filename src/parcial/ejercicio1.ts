eda803c4a60182678cc2ebb952cdc152c3c05203
Yadir <yadamuoz@misena.edu.co>
2024-10-23 22:38:20 -0500

    punto 1 implementado

---
+++ src/parcial/ejercicio1.ts
@@ -0,0 +1,11 @@
+  // genere un triangulo de numeros con 5 filas utilizando ciclos for anidados.
+ const arrayNumberTriangle = [];
+ for (let i = 1; i <= 5; i++) {
+     let row = [];
+     for (let j = 1; j <= i; j++) {
+         row.push(j);
+     }
+     arrayNumberTriangle.push(row);
+ }
+ console.log(arrayNumberTriangle, 'arrayNumberTriangle');
+
